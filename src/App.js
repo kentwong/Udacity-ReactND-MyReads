775c3bca5c6eb16e5c2a7d51f9031647c875fc1a
kentwong <kent_wong@outlook.com>
2020-07-15 09:22:04 +1000

    create static data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+import getAll from "./data";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
 
-class BooksApp extends React.Component {
-  state = {};
+class BooksApp extends Component {
+  bookshelves = [
+    { key: "currentlyReading", name: "Currently Reading" },
+    { key: "wantToRead", name: "Want to Read" },
+    { key: "read", name: "Have Read" },
+  ];
+  state = {
+    books: getAll,
+  };
 
   render() {
     return (
@@ -16,49 +24,110 @@ class BooksApp extends React.Component {
   }
 }
 
-class BookList extends Component {
+class ListBooks extends Component {
   render() {
+    const { bookshelves } = this.props;
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
+        <Bookcase bookshelves={bookshelves} />
+        <OpenSearchButton />
+      </div>
+    );
+  }
+}
+
+const OpenSearchButton = () => {
+  return (
+    <div className="open-search">
+      <Link to="search">
+        <button>Add a book</button>
+      </Link>
+    </div>
+  );
+};
+
+const Bookcase = (props) => {
+  const { bookshelves } = props;
+  return (
+    <div className="list-books-content">
+      <div>
+        {bookshelves.map((shelf) => (
+          <Bookshelf key={shelf.key} shelf={shelf} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Bookshelf = (props) => {
+  const { shelf } = props;
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelf.name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          <Book book={{}} />
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+const Book = (props) => {
+  const { book } = props;
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage:
+                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+            }}
+          ></div>
+          <BookshelfCharger />
+        </div>
+        <div className="book-title">To Kill a Mockingbird</div>
+        <div className="book-authors">Harper Lee</div>
+      </div>
+    </li>
+  );
+};
+
+class BookshelfCharger extends Component {
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="list-books">
         <div className="list-books-content">
           <div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div
-                          className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage:
-                              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                          }}
-                        ></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>
-                              Move to...
-                            </option>
-                            <option value="currentlyReading">
-                              Currently Reading
-                            </option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">To Kill a Mockingbird</div>
-                      <div className="book-authors">Harper Lee</div>
-                    </div>
-                  </li>
                   <li>
                     <div className="book">
                       <div className="book-top">
@@ -264,11 +333,6 @@ class BookList extends Component {
             </div>
           </div>
         </div>
-        <div className="open-search">
-          <button onClick={() => this.setState({ showSearchPage: true })}>
-            Add a book
-          </button>
-        </div>
       </div>
     );
   }
@@ -278,31 +342,48 @@ class BookSearch extends Component {
   render() {
     return (
       <div className="search-books">
-        <div className="search-books-bar">
-          <button
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
-          >
-            Close
-          </button>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-    
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+        <SearchBar />
+        <SearchResults />
       </div>
     );
   }
 }
+
+const SearchBar = (props) => {
+  return (
+    <div className="search-books-bar">
+      <CloseSearchButton />
+      <SearchBooksInput />
+    </div>
+  );
+};
+
+const CloseSearchButton = () => {
+  return (
+    <Link to="/">
+      <button className="close-search">Close</button>
+    </Link>
+  );
+};
+
+class SearchBooksInput extends Component {
+  render() {
+    return (
+      <div className="search-books-input-wrapper">
+        <input type="text" placeholder="Search by title or author" />
+      </div>
+    );
+  }
+}
+
+const SearchResults = (props) => {
+  return (
+    <div className="search-books-results">
+      <ol className="books-grid">
+        <Book />
+      </ol>
+    </div>
+  );
+};
 
 export default BooksApp;
